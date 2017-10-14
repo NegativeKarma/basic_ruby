d7694b1e4b192cbd7cba2ed90a27014cb734455b
Francesco <francesco.falchi@gmail.com>
2017-10-14 18:15:00 +0200

    Folder creation

---
+++ calc.rb
@@ -0,0 +1,57 @@
+puts 1+2
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -52
+puts 24 * 365
+puts 60 * 24 * 3650
+puts (60 * 60) * 24 * 365 *34
+puts 1298000000 / 60 / 60 / 24 / 365
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'ciao ' * 4
+puts 'I\'m here!'
+puts 'destra\\sinistra'
+puts 'destra\sinistra'
+puts myString = "...even a broken clock it\'s right twice a day"
+puts myString
+puts myString
+puts name = 'Jessica Rabbit Donald Duck McScrooge'
+puts 'Her name was ' + name + '!'
+puts 'I go by the name of ' + name + ', it\'s a long one'
+puts car = 'Renault'
+puts 'I like ' + car + 's'
+puts car = 'Ferrari'
+puts 'I like ' + car + 's more'
+puts var = 'again ' + 'and again'
+puts var
+puts math = 5 * (3-12)
+puts math
+puts 'What time is it?'
+time = gets
+puts 'It is ' + time + 'right now'
+puts 'What time is it?'
+time = gets.chomp
+puts 'It is ' + time + ' right now'
+puts 'What\'s your name?'
+name1 = gets.chomp
+puts 'And your middle name?'
+middle_name = gets.chomp
+puts 'Last but not least, your last name?'
+last_name = gets.chomp
+puts 'Hello ' + name1 + ' ' + middle_name + ' ' + last_name + ' and welcome to hell!!!'
+puts 'What\'s your favourite number?'
+number = gets.chomp
+puts number.to_i + 1
+puts 'is way better'
+number = gets.chomp ; puts "Your number is #{number.to_i}, but #{number.to_i + 1} would have been way better"
---
+++ my_group.rb
@@ -0,0 +1,9 @@
+my_group = Array.new
+
+person_1 = {name: "Bob", gender: "m", age: 24}
+
+person_2 = {name: "Lucy", gender: "f", age: 32}
+
+person_3 = {name: "Ken", gender: "m", age: 64}
+
+my_group = ["Bob", "Lucy", "Ken"]
