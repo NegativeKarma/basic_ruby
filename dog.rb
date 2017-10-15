90813a48a225f6350f6bd455284e543a2ab323d4
Francesco <francesco.falchi@gmail.com>
2017-10-15 22:21:48 +0200

    add dog.rb

---
+++ dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  attr_accessor :breed, :name
+
+  def initialize(breed, name)
+    @breed = breed
+    @name = name
+  end
+
+dog = Dog.new('doberman', 'klaus')
+
+  def wag_tail
+    puts "Tail wagging"
+  end
+  def barking
+    puts "Barks at the moon"
+  end
+  def tag_address owner: "Scott", street: "7th avenue", number: 78
+    puts owner
+    puts street
+    puts number
+  end
+
+end
